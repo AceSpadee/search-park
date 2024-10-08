b4c0e9b5d0ea7c4780fd13e3f33275cc539a26fe
AceSpadee <148384493+AceSpadee@users.noreply.github.com>
2024-10-07 22:33:43 -0700

    added satellite images

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -3,11 +3,21 @@ import 'leaflet/dist/leaflet.css';
 
 const MapComponent = ({ locations }) => {
   return (
-    <MapContainer center={[45.6280, -122.6739]} zoom={10} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={[45.6280, -122.6739]} zoom={12} style={{ height: '400px', width: '100%' }}>
+      {/* Esri Satellite Imagery as the base layer */}
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        attribution="Tiles &copy; Esri &mdash; Source: Esri, USGS, NOAA"
       />
 
+      {/* OpenStreetMap roads and labels as a semi-transparent overlay */}
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+        opacity={0.4}
+      />
+
+      {/* Render the markers passed as locations */}
       {locations.map((location, index) => (
         <Marker key={index} position={[location.lat, location.lng]}>
           <Popup>
