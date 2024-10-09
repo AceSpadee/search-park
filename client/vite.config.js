beb5e8ef7f5cef85f61861415a3c4e8069137104
AceSpadee <148384493+AceSpadee@users.noreply.github.com>
2024-10-09 15:32:16 -0700

    fixing variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000,
+    host: '0.0.0.0', // Ensure Vite binds to 0.0.0.0 for external access
+    port: process.env.PORT || 3000, // Use Render's dynamic port or fallback to 3000 locally
     proxy: {
       '/api': {
-        target: '0.0.0.0',
+        target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true,
       },
