5d80b3e4be5211c1c2905f289371bdbbddc8a1da
AceSpadee <148384493+AceSpadee@users.noreply.github.com>
2024-11-12 17:11:03 -0800

    added styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,29 +11,29 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
 
   return (
     <nav className="navbar">
-      <div className="nav-section nav-links">
-        <Link to="/map" className="nav-button map-btn">Map</Link>
-        {!isLoggedIn && (
-          <>
-            <Link to="/login" className="nav-button">Login</Link>
-            <Link to="/register" className="nav-button">Register</Link>
-          </>
-        )}
-        {isLoggedIn && (
-          <Logout handleLogout={handleLogout} className="nav-button logout-button" />
-        )}
+      {/* Home Button */}
+      <div className="nav-section">
+        <Link to="/" className="nav-button home-btn">Home</Link>
       </div>
 
+      {/* Locations Button */}
       <div className="nav-section logo">
         <Link to="/location" className="nav-button">Locations</Link>
       </div>
 
-      <div className="nav-section">
-        <Link to="/" className="nav-button home-btn">Home</Link>
+      {/* Map, Login/Register or Logout */}
+      <div className="nav-section nav-links">
+        {!isLoggedIn ? (
+          <>
+            <Link to="/login" className="nav-button">Login</Link>
+            <Link to="/register" className="nav-button">Register</Link>
+          </>
+        ) : (
+          <Logout handleLogout={handleLogout} className="nav-button logout-button" />
+        )}
       </div>
     </nav>
   );
 };
-
 
 export default Navbar;
